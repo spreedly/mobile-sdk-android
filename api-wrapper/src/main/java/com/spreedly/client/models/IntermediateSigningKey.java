3722a24ea83e9bad30b0f911f5903b3ca5b69b76
Stefan Rusek <stefan@rusek.org>
2020-05-06 19:38:28 -0400

    fix lint

--- api-wrapper/src/main/java/com/spreedly/client/models/IntermediateSigningKey.java
+++ api-wrapper/src/main/java/com/spreedly/client/models/IntermediateSigningKey.java
@@ -8,7 +8,7 @@ public class IntermediateSigningKey {
     @NonNull
     public final String[] signatures;
 
-    public IntermediateSigningKey(String signedKey, String[] signatures) {
+    public IntermediateSigningKey(@NonNull String signedKey, @NonNull String[] signatures) {
         this.signatures = signatures;
         this.signedKey = signedKey;
     }
