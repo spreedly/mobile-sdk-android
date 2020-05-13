c7ef6e127bda78a9cb27e57989b3c3fbb6596f53
Clarice Costello <clarice@ergonlabs.com>
2020-05-13 15:04:04 -0700

    linter

--- api-wrapper/src/main/java/com/spreedly/client/models/RecacheInfo.java
+++ api-wrapper/src/main/java/com/spreedly/client/models/RecacheInfo.java
@@ -2,10 +2,13 @@ package com.spreedly.client.models;
 
 import org.json.JSONObject;
 
+import io.reactivex.rxjava3.annotations.NonNull;
+
 public class RecacheInfo {
+    @NonNull
     public final JSONObject json;
 
-    public RecacheInfo(SpreedlySecureOpaqueString cvv) {
+    public RecacheInfo(@NonNull SpreedlySecureOpaqueString cvv) {
         JSONObject wrapper = new JSONObject();
         JSONObject paymentMethod = new JSONObject();
         JSONObject creditCard = new JSONObject();
