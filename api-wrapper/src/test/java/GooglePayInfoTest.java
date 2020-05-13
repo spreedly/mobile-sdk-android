979eddc26016a9e8c0943200037356a19f667efd
Clarice Costello <clarice@ergonlabs.com>
2020-05-13 10:24:45 -0700

    google pay tests

--- api-wrapper/src/test/java/GooglePayInfoTest.java
+++ api-wrapper/src/test/java/GooglePayInfoTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class GooglePlayInfoTest {
+public class GooglePayInfoTest {
     @Test
     public void CanCreateGooglePayInfo() {
         GooglePayInfo googlePay = new GooglePayInfo("Jane", "Doe", "sample signature", "sample version", "sample signing key", false);
