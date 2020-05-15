562b13471d2401f83fba3d738cf0ea9e803f3ac7
Clarice Costello <clarice@ergonlabs.com>
2020-05-15 10:52:58 -0700

    google pay with current best payload

--- sdk_sample/src/main/java/com/spreedly/sdk_sample/simple/GooglePayFragmentViewModel.java
+++ sdk_sample/src/main/java/com/spreedly/sdk_sample/simple/GooglePayFragmentViewModel.java
@@ -23,7 +23,17 @@ public class GooglePayFragmentViewModel extends ViewModel {
 
     void create() {
         final SpreedlyClient client = SpreedlyClient.newInstance("", "", true);
-        final GooglePayInfo info = new GooglePayInfo(name.getValue(), "", "", "", "", false);
+        final String[] names = name.getValue().split(" ", 2);
+        final String paymentData = "{\n" +
+                "  \"protocolVersion\":\"ECv2\",\n" +
+                "  \"signature\":\"MEQCIH6Q4OwQ0jAceFEkGF0JID6sJNXxOEi4r+mA7biRxqBQAiAondqoUpU/bdsrAOpZIsrHQS9nwiiNwOrr24RyPeHA0Q\\u003d\\u003d\",\n" +
+                "  \"intermediateSigningKey\":{\n" +
+                "    \"signedKey\": \"{\\\"keyExpiration\\\":\\\"1542323393147\\\",\\\"keyValue\\\":\\\"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE/1+3HBVSbdv+j7NaArdgMyoSAM43yRydzqdg1TxodSzA96Dj4Mc1EiKroxxunavVIvdxGnJeFViTzFvzFRxyCw\\\\u003d\\\\u003d\\\"}\",\n" +
+                "    \"signatures\": [\"MEYCIQCO2EIi48s8VTH+ilMEpoXLFfkxAwHjfPSCVED/QDSHmQIhALLJmrUlNAY8hDQRV/y1iKZGsWpeNmIP+z+tCQHQxP0v\"]\n" +
+                "  },\n" +
+                "  \"signedMessage\":\"{\\\"tag\\\":\\\"jpGz1F1Bcoi/fCNxI9n7Qrsw7i7KHrGtTf3NrRclt+U\\\\u003d\\\",\\\"ephemeralPublicKey\\\":\\\"BJatyFvFPPD21l8/uLP46Ta1hsKHndf8Z+tAgk+DEPQgYTkhHy19cF3h/bXs0tWTmZtnNm+vlVrKbRU9K8+7cZs\\\\u003d\\\",\\\"encryptedMessage\\\":\\\"mKOoXwi8OavZ\\\"}\"\n" +
+                "}";
+        final GooglePayInfo info = new GooglePayInfo(names[0], names[1], paymentData, false);
         info.testCardNumber = "411111111111111";
         inProgress.setValue(true);
         token.postValue("");
