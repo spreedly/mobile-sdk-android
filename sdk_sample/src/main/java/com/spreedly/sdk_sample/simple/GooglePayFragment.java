0248e8e1ef72dcd045be374fee0030300fcecf34
Clarice Costello <clarice@ergonlabs.com>
2020-05-15 10:22:41 -0700

    basic google pay fragment

--- sdk_sample/src/main/java/com/spreedly/sdk_sample/simple/GooglePayFragment.java
+++ sdk_sample/src/main/java/com/spreedly/sdk_sample/simple/GooglePayFragment.java
@@ -4,15 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.spreedly.sdk_sample.R;
 
-public class GooglePayFragment extends Fragment {
+public class GooglePayFragment extends BillingFragmentBase {
 
     private GooglePayFragmentViewModel mViewModel;
 
@@ -30,7 +31,20 @@ public class GooglePayFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(GooglePayFragmentViewModel.class);
-        // TODO: Use the ViewModel
+
+        bindEditText(getView().findViewById(R.id.name), mViewModel.name);
+
+        bindTextView(getView().findViewById(R.id.token), mViewModel.token);
+        bindTextView(getView().findViewById(R.id.error), mViewModel.error);
+
+        mViewModel.inProgress.observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean inProgress) {
+                getView().findViewById(R.id.name).setEnabled(!inProgress);
+            }
+        });
+
+        getView().<Button>findViewById(R.id.submit).setOnClickListener(b -> mViewModel.create());
     }
 
 }
--- sdk_sample/src/main/java/com/spreedly/sdk_sample/simple/GooglePayFragmentViewModel.java
+++ sdk_sample/src/main/java/com/spreedly/sdk_sample/simple/GooglePayFragmentViewModel.java
@@ -1,8 +1,60 @@
 package com.spreedly.sdk_sample.simple;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.spreedly.client.SpreedlyClient;
+import com.spreedly.client.models.GooglePayInfo;
+import com.spreedly.client.models.results.PaymentMethodResult;
+import com.spreedly.client.models.results.TransactionResult;
+
+import io.reactivex.rxjava3.core.SingleObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 
 public class GooglePayFragmentViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    MutableLiveData<String> name = new MutableLiveData<>("");
+    MutableLiveData<String> token = new MutableLiveData<>(null);
+    MutableLiveData<String> error = new MutableLiveData<>(null);
+    MutableLiveData<Boolean> inProgress = new MutableLiveData<>(false);
+
+    void create() {
+        final SpreedlyClient client = SpreedlyClient.newInstance("", "", true);
+        final GooglePayInfo info = new GooglePayInfo(name.getValue(), "", "", "", "", false);
+        info.testCardNumber = "411111111111111";
+        inProgress.setValue(true);
+        token.postValue("");
+        error.postValue("");
+        client.createGooglePaymentMethod(info, null, null).subscribe(new SingleObserver<TransactionResult<PaymentMethodResult>>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onSuccess(TransactionResult<PaymentMethodResult> trans) {
+                try {
+                    if (trans.succeeded) {
+                        Log.i("Spreedly", "trans.result.token: " + trans.result.token);
+                        token.postValue(trans.result.token);
+                    } else {
+                        Log.e("Spreedly", "trans.message: " + trans.message);
+                        error.postValue(trans.message);
+                    }
+                } finally {
+                    inProgress.postValue(false);
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e("Spreedly", e.getMessage(), e);
+                error.postValue("UNEXPECTED ERROR: " + e.getMessage());
+                inProgress.postValue(false);
+            }
+        });
+    }
 }
