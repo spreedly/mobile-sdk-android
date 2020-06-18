46a34f2f3b0b4febb1764c703521c222d138da24
Clarice Costello <clarice@ergonlabs.com>
2020-06-18 14:49:36 -0700

    WIP address inputs

---
+++ express/src/main/java/com/spreedly/express/AddressFieldView.java
@@ -0,0 +1,112 @@
+package com.spreedly.express;
+
+import android.content.Context;
+import android.text.InputType;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class AddressFieldView extends LinearLayout {
+    AddressType addressType;
+    TextInputLayout address1Wrapper;
+    TextInputEditText address1Content;
+
+    TextInputLayout address2Wrapper;
+    TextInputEditText address2Content;
+
+    TextInputLayout cityWrapper;
+    TextInputEditText cityContent;
+
+    TextInputLayout stateWrapper;
+    TextInputEditText stateContent;
+
+    TextInputLayout zipWrapper;
+    TextInputEditText zipContent;
+
+    Spinner country;
+
+    TextInputLayout phoneWrapper;
+    TextInputEditText phoneContent;
+
+    public AddressFieldView(@NonNull Context context, @NonNull AddressType addressType) {
+        super(context);
+        this.addressType = addressType;
+    }
+
+    @Override
+    public void onFinishInflate() {
+        super.onFinishInflate();
+        init();
+    }
+
+    void init() {
+        this.setOrientation(VERTICAL);
+
+        address1Wrapper = new TextInputLayout(this.getContext());
+        address1Content = new TextInputEditText(this.getContext());
+        address1Wrapper.setHint("Address Line 1");
+        address1Wrapper.addView(address1Content);
+
+
+        address2Wrapper = new TextInputLayout(this.getContext());
+        address2Content = new TextInputEditText(this.getContext());
+        address2Wrapper.setHint("Line 2 (Apt, Unit, etc)");
+        address2Wrapper.addView(address2Content);
+
+        cityWrapper = new TextInputLayout(this.getContext());
+        cityContent = new TextInputEditText(this.getContext());
+        cityWrapper.setHint("City");
+        cityWrapper.addView(cityContent);
+
+        stateWrapper = new TextInputLayout(this.getContext());
+        stateContent = new TextInputEditText(this.getContext());
+        stateWrapper.setHint("State/Province");
+        stateWrapper.addView(stateContent);
+
+        ArrayAdapter countryAdapter = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.country_array, android.R.layout.simple_spinner_item);
+        country = new Spinner(this.getContext());
+        country.setAdapter(countryAdapter);
+
+        zipWrapper = new TextInputLayout(this.getContext());
+        zipWrapper.setHint("Zipcode");
+        zipContent = new TextInputEditText(this.getContext());
+        zipContent.setInputType(InputType.TYPE_CLASS_PHONE);
+
+        if (addressType == AddressType.BILLING) {
+            address1Wrapper.setId(R.id.spreedly_address1);
+            address2Wrapper.setId(R.id.spreedly_address2);
+            cityWrapper.setId(R.id.spreedly_city);
+            stateWrapper.setId(R.id.spreedly_state);
+            zipWrapper.setId(R.id.spreedly_zip);
+            country.setId(R.id.spreedly_country);
+            phoneWrapper.setId(R.id.spreedly_phone_number);
+        } else {
+            address1Wrapper.setId(R.id.spreedly_shipping_address1);
+            address2Wrapper.setId(R.id.spreedly_shipping_address2);
+            cityWrapper.setId(R.id.spreedly_shipping_city);
+            stateWrapper.setId(R.id.spreedly_shipping_state);
+            zipWrapper.setId(R.id.spreedly_shipping_zip);
+            country.setId(R.id.spreedly_shipping_country);
+            phoneWrapper.setId(R.id.spreedly_shipping_phone_number);
+        }
+
+        this.addView(address1Wrapper);
+        this.addView(address2Wrapper);
+        this.addView(cityWrapper);
+        this.addView(stateWrapper);
+        this.addView(zipWrapper);
+        this.addView(country);
+    }
+
+    public enum AddressType {BILLING, SHIPPING}
+}
+
--- express/src/main/java/com/spreedly/express/ExpressCreditCardView.java
+++ express/src/main/java/com/spreedly/express/ExpressCreditCardView.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ScrollView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,19 +13,22 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.spreedly.securewidgets.SecureCreditCardField;
 import com.spreedly.securewidgets.SecureExpirationDate;
+import com.spreedly.securewidgets.SecureFormLayout;
 import com.spreedly.securewidgets.SecureTextField;
 
 /**
  * TODO: document your custom view class.
  */
-public class ExpressCreditCardView extends LinearLayout {
+public class ExpressCreditCardView extends ScrollView {
+    SecureFormLayout layoutWrapper;
     TextInputLayout fullNameWrapper;
     TextInputEditText fullNameContent;
     SecureCreditCardField secureCreditCardField;
     SecureTextField ccvField;
     SecureExpirationDate secureExpirationDate;
     Button submitButton;
-    TextView sample;
+    AddressFieldView billingAddress;
+    AddressFieldView shippingAddress;
 
     public ExpressCreditCardView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -38,25 +41,37 @@ public class ExpressCreditCardView extends LinearLayout {
     }
 
     public void init() {
-        fullNameWrapper = new TextInputLayout(this.getContext());
-        fullNameContent = new TextInputEditText(this.getContext());
+        layoutWrapper = new SecureFormLayout(this.getContext());
+        layoutWrapper.setOrientation(LinearLayout.VERTICAL);
+        fullNameWrapper = new TextInputLayout(layoutWrapper.getContext());
+        fullNameContent = new TextInputEditText(layoutWrapper.getContext());
         fullNameWrapper.setHint("Full Name");
         fullNameWrapper.addView(fullNameContent);
-        secureCreditCardField = new SecureCreditCardField(this.getContext());
+
+        secureCreditCardField = new SecureCreditCardField(layoutWrapper.getContext());
         secureCreditCardField.setId(R.id.spreedly_credit_card_number);
         secureCreditCardField.onFinishInflate();
-        ccvField = new SecureTextField(this.getContext());
+
+        ccvField = new SecureTextField(layoutWrapper.getContext());
         ccvField.setId(R.id.spreedly_ccv);
         ccvField.onFinishInflate();
-        secureExpirationDate = new SecureExpirationDate(this.getContext());
+
+        secureExpirationDate = new SecureExpirationDate(layoutWrapper.getContext());
         secureExpirationDate.setId(R.id.spreedly_cc_expiration_date);
-        submitButton = new Button(this.getContext());
+
+        billingAddress = new AddressFieldView(layoutWrapper.getContext());
+        billingAddress.onFinishInflate();
+
+        submitButton = new Button(layoutWrapper.getContext());
         submitButton.setText("Submit");
-        this.addView(fullNameWrapper);
-        this.addView(secureCreditCardField);
-        this.addView(ccvField);
-        this.addView(secureExpirationDate);
-        this.addView(submitButton);
+        layoutWrapper.addView(fullNameWrapper);
+        layoutWrapper.addView(secureCreditCardField);
+        layoutWrapper.addView(ccvField);
+        layoutWrapper.addView(secureExpirationDate);
+        layoutWrapper.addView(billingAddress);
+        layoutWrapper.addView(submitButton);
+        layoutWrapper.onFinishInflate();
+        this.addView(layoutWrapper);
 
     }
 }
